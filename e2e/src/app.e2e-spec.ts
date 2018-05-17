d6aa510d26aa49030a1b3915594155f268be42de
tiborbotos <tibor.botos@gmail.com>
2018-05-17 14:04:30 +0200

    Added some e2e tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, by, element, protractor } from 'protractor';
 
 describe('workspace-project App', () => {
     let page: AppPage;
@@ -13,11 +14,19 @@ describe('workspace-project App', () => {
     });
 
     it('should be able to search for a github repository', () => {
+        element(by.css('input')).sendKeys('angular/angular');
+
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(by.css('.result-count'))));
+
+        expect(element(by.css('.result-count')).getText()).toContain('repository, displayed 30');
+        expect(element.all(by.css('.item')).get(1).getText()).toContain('angular/angular');
     });
 
     it('should load issues', () => {
-    });
+        element.all(by.css('.details--issues')).get(1).click();
 
-    it('should paginate issues', () => {
+        browser.wait(protractor.ExpectedConditions.visibilityOf(element(by.css('ghr-repository-details'))));
+
+        expect(element.all(by.css('ghr-repository-details .issue')).count()).toBeGreaterThan(0);
     });
 });
